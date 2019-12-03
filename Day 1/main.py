b768ee95e0066d654cb6e80ef7646e5c7c9a1da8
Kai <kaicharlton@gmail.com>
2019-12-03 14:03:11 -0600

    refactor to take fuel list,  print sum

--- Day 1/main.py
+++ Day 1/main.py
@@ -3,9 +3,13 @@ import math
 
 
 def main():
-    mass = int(sys.argv[1])
-    fuel = math.floor(mass / 3) - 2
-    print(fuel)
+    filename = sys.argv[1]
+    module_masses = open(filename).read().split('\n')
+    del module_masses[-1]
+
+    module_masses = list(map(int, module_masses))
+    fuel_list = [(math.floor(mass/3) - 2) for mass in module_masses]
+    print(sum(fuel_list))
 
 
 if __name__ == "__main__":
