0242af50ca1a51fa262802e96092ff966f789530
Kai <kaicharlton@gmail.com>
2019-12-03 14:50:39 -0600

    recursively calculate fuel needed

--- Day 1/2/main.py
+++ Day 1/2/main.py
@@ -8,8 +8,16 @@ def main():
     del module_masses[-1]
 
     module_masses = list(map(int, module_masses))
-    fuel_list = [(math.floor(mass/3) - 2) for mass in module_masses]
+    fuel_list = [calc_fuel(mass) for mass in module_masses]
     print(sum(fuel_list))
+
+
+def calc_fuel(mass):
+    if mass <= 5:
+        return 0
+    else:
+        fuel = math.floor(mass/3) - 2
+        return fuel + calc_fuel(fuel)
 
 
 if __name__ == "__main__":
