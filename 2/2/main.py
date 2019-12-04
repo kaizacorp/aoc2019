f57d6f97d0b0e472e666e6c74b0c2b50a4831727
Kai <kaicharlton@gmail.com>
2019-12-04 17:05:45 -0600

    compute random pair (one iteration)

--- 2/2/main.py
+++ 2/2/main.py
@@ -1,4 +1,5 @@
 import sys
+from random import randint
 
 
 def main():
@@ -6,38 +7,64 @@ def main():
     Intcode = open(filename).read().split(',')
     Intcode.append(Intcode.pop().rstrip())
     Intcode = list(map(int, Intcode))
+    initial_state = Intcode.copy()
     # print(Intcode)
+
+    used = []
+    noun = 0
+    verb = 0
+
+    #   fix 'noun' and 'verb' as random integer (1-99) -> one not in tracked list
+    is_new_pair = False
+    while not is_new_pair:
+        noun = randint(1, 99)
+        verb = randint(1, 99)
+        pair = (noun, verb)
+        if pair not in used:
+            is_new_pair = True
+            #   track tuple in list
+            used.append(pair)
+    #   reset to initial state
+    Intcode = initial_state.copy()
+    #   assign new noun/verb values to state
+    Intcode[1] = noun
+    Intcode[2] = verb
+
+    #   compute through opcodes
 
     index = 0
     opcode = Intcode[index]
     while opcode != 99:
         if opcode == 1:
-            """
-            Opcode 1 adds together numbers read from two positions and stores the result in a third position. The three integers immediately after the opcode tell you these three positions - the first two indicate the positions from which you should read the input values, and the third indicates the position at which the output should be stored.
-            """
             a = Intcode[Intcode[index+1]]
             b = Intcode[Intcode[index+2]]
             dest = Intcode[index+3]
             Intcode[dest] = a + b
         elif opcode == 2:
-            """
-            Opcode 2 works exactly like opcode 1, except it multiplies the two inputs instead of adding them. Again, the three integers after the opcode indicate where the inputs and outputs are, not their values.
-            """
             a = Intcode[Intcode[index+1]]
             b = Intcode[Intcode[index+2]]
             dest = Intcode[index+3]
             Intcode[dest] = a * b
 
-        """
-        Once you're done processing an opcode, move to the next one by stepping forward 4 positions.
-        """
         index += 4
         opcode = Intcode[index]
 
     print(Intcode[0])
-    #Intcode = list(map(str, Intcode))
-    #result = ",".join(Intcode)
-    # print(result)
+
+    #   check if final value at position 0 is 19690720
+    if Intcode[0] == 19690720:
+        #   if true, print 100 * noun + verb
+        print("Found!")
+        print("Noun: ", noun, "Verb: ", verb)
+        print(100 * noun + verb)
+    else:
+        #   if not, retry with new tuple
+        print("Noun: ", noun, "Verb: ", verb)
+
+
+def print_as_input(Intcode):
+    result = ",".join(list(map(str, Intcode)))
+    print(result)
 
 
 if __name__ == "__main__":
