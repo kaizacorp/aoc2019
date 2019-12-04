032b6313c7485cae7eacf9445b2862f52c1db21c
Kai <kaicharlton@gmail.com>
2019-12-04 17:37:41 -0600

    search until the right noun/verb  is found

--- 2/2/main.py
+++ 2/2/main.py
@@ -13,53 +13,57 @@ def main():
     used = []
     noun = 0
     verb = 0
+    searching = True
 
-    #   fix 'noun' and 'verb' as random integer (1-99) -> one not in tracked list
-    is_new_pair = False
-    while not is_new_pair:
-        noun = randint(1, 99)
-        verb = randint(1, 99)
-        pair = (noun, verb)
-        if pair not in used:
-            is_new_pair = True
-            #   track tuple in list
-            used.append(pair)
-    #   reset to initial state
-    Intcode = initial_state.copy()
-    #   assign new noun/verb values to state
-    Intcode[1] = noun
-    Intcode[2] = verb
+    while searching:
 
-    #   compute through opcodes
+        #   fix 'noun' and 'verb' as random integer (1-99) -> one not in tracked list
+        is_new_pair = False
+        while not is_new_pair:
+            noun = randint(1, 99)
+            verb = randint(1, 99)
+            pair = (noun, verb)
+            if pair not in used:
+                is_new_pair = True
+                #   track tuple in list
+                used.append(pair)
+        #   reset to initial state
+        Intcode = initial_state.copy()
+        #   assign new noun/verb values to state
+        Intcode[1] = noun
+        Intcode[2] = verb
 
-    index = 0
-    opcode = Intcode[index]
-    while opcode != 99:
-        if opcode == 1:
-            a = Intcode[Intcode[index+1]]
-            b = Intcode[Intcode[index+2]]
-            dest = Intcode[index+3]
-            Intcode[dest] = a + b
-        elif opcode == 2:
-            a = Intcode[Intcode[index+1]]
-            b = Intcode[Intcode[index+2]]
-            dest = Intcode[index+3]
-            Intcode[dest] = a * b
+        #   compute through opcodes
 
-        index += 4
+        index = 0
         opcode = Intcode[index]
+        while opcode != 99:
+            if opcode == 1:
+                a = Intcode[Intcode[index+1]]
+                b = Intcode[Intcode[index+2]]
+                dest = Intcode[index+3]
+                Intcode[dest] = a + b
+            elif opcode == 2:
+                a = Intcode[Intcode[index+1]]
+                b = Intcode[Intcode[index+2]]
+                dest = Intcode[index+3]
+                Intcode[dest] = a * b
 
-    print(Intcode[0])
+            index += 4
+            opcode = Intcode[index]
 
-    #   check if final value at position 0 is 19690720
-    if Intcode[0] == 19690720:
-        #   if true, print 100 * noun + verb
-        print("Found!")
-        print("Noun: ", noun, "Verb: ", verb)
-        print(100 * noun + verb)
-    else:
-        #   if not, retry with new tuple
-        print("Noun: ", noun, "Verb: ", verb)
+        print(Intcode[0])
+
+        #   check if final value at position 0 is 19690720
+        if Intcode[0] == 19690720:
+            #   if true, print 100 * noun + verb
+            searching = False
+            print("Found!")
+            print("Noun: ", noun, "Verb: ", verb)
+            print(100 * noun + verb)
+        else:
+            #   if not, retry with new tuple
+            print("Noun: ", noun, "Verb: ", verb)
 
 
 def print_as_input(Intcode):
