2eed13d673f655c55c4342cba32635a64ebaf76f
Kai <kaicharlton@gmail.com>
2019-12-04 11:18:57 -0600

    input as list of values, template for opcode logic

--- 2/1/main.py
+++ 2/1/main.py
@@ -1,4 +1,29 @@
+import sys
+
+
 def main():
+    filename = sys.argv[1]
+    Intcode = open(filename).read().split(',')
+    Intcode.append(Intcode.pop().rstrip())
+    print(Intcode)
+
+    index = 0
+    opcode = Intcode[index]
+    while opcode != 99:
+        if opcode == 1:
+            """
+            Opcode 1 adds together numbers read from two positions and stores the result in a third position. The three integers immediately after the opcode tell you these three positions - the first two indicate the positions from which you should read the input values, and the third indicates the position at which the output should be stored.
+            """
+        elif opcode == 2:
+            """
+            Opcode 2 works exactly like opcode 1, except it multiplies the two inputs instead of adding them. Again, the three integers after the opcode indicate where the inputs and outputs are, not their values.
+            """
+
+        """
+        Once you're done processing an opcode, move to the next one by stepping forward 4 positions.
+        """
+        index += 4
+        opcode = Intcode[index]
 
 
 if __name__ == "__main__":
