f085e3a5a25b908e27a6564e6d46f91eacb661df
Kai <kaicharlton@gmail.com>
2019-12-05 17:12:49 -0600

    not in larger group by  counting for doubles

---
+++ 4/2/main.py
@@ -0,0 +1,51 @@
+import sys
+
+
+def main():
+    filename = sys.argv[1]
+    number_range = open(filename).read().split('\n')
+    lower = int(number_range[0].split("-")[0])
+    upper = int(number_range[0].split("-")[1])
+
+    print(lower, upper)
+
+    valid = []
+
+    for password in range(lower, upper):
+        doubles = has_double_digits(password)
+        increasing = is_increasing_digits(password)
+        if doubles and increasing:
+            valid.append(password)
+
+    not_in_larger_group = []
+    for password in valid:
+        password = str(password)
+        count = {}
+        for chars in password:
+            count[chars] = password.count(chars)
+        if 2 in count.values():
+            not_in_larger_group.append(password)
+
+    print(len(not_in_larger_group))
+
+
+def has_double_digits(integer):
+    integer = str(integer)
+    if '00' in integer or '11' in integer or '22' in integer or '33' in integer or '44' in integer or '55' in integer or '66' in integer or '77' in integer or '88' in integer or '99' in integer:
+        return True
+    return False
+
+
+def is_increasing_digits(integer):
+    integer = str(integer)
+    increasing = True
+
+    for first, second in zip(integer[:-1], integer[1:]):
+        if int(first) > int(second):
+            increasing = False
+
+    return increasing
+
+
+if __name__ == "__main__":
+    main()
