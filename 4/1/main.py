ea4c58a376ac672a54dc55a73534c3b3dfb675af
Kai <kaicharlton@gmail.com>
2019-12-05 14:40:17 -0600

    check for double digits

--- 4/1/main.py
+++ 4/1/main.py
@@ -8,6 +8,20 @@ def main():
     upper = int(number_range[0].split("-")[1])
 
     print(lower, upper)
+    valid = []
+
+    for password in range(lower, upper):
+        if has_double_digits(password) and is_increasing_digits(password):
+            valid.append(password)
+
+    print(len(valid))
+
+
+def has_double_digits(integer):
+    integer = str(integer)
+    if '00' in integer or '11' in integer or '22' in integer or '33' in integer or '44' in integer or '55' in integer or '66' in integer or '77' in integer or '88' in integer or '99' in integer:
+        return True
+    return False
 
 
 if __name__ == "__main__":
